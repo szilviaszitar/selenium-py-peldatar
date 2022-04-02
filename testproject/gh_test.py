575a8d0499c5ba7fdffe12aa4a3ce84e131ee3b8
Szilvia Szitár <97233944+szilviaszitar@users.noreply.github.com>
2022-04-03 01:54:55 +0200

    Update gh_test.py

--- testproject/gh_test.py
+++ testproject/gh_test.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 # driver = webdriver.Firefox()
 driver = webdriver.Chrome()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
+elem = driver.find_element(By.NAME, "q")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
